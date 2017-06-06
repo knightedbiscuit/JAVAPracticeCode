ba9e28ce08317e3393582fe9936032db0b9bd858
knghtedbiiscuit <alibiscuit@gmail.com>
2017-06-06 12:58:13 +0800

    new anotherdayservlet project

---
+++ AnotherDayServlet/src/controllor/PostServlet.java
@@ -0,0 +1,42 @@
+package controllor;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PostServlet
+ */
+@WebServlet(name = "postservlet", urlPatterns = { "/postservlet" })
+public class PostServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PostServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		String nick = request.getParameter("nick");
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html><h1> This was your second movie Servlet</h1><ul>");
+		out.println("<li> Name is "+name);
+		out.println("<li> Nick Name is "+ nick);
+		out.println("</ul></html>");
+		
+	}
+
+}
---
+++ AnotherDayServlet/src/controllor/mysecondservlet.java
@@ -0,0 +1,44 @@
+package controllor;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class mysecondservlet
+ */
+@WebServlet(name = "servlet2", urlPatterns = { "/servlet2" })
+public class mysecondservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public mysecondservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RequestDispatcher rd = request.getRequestDispatcher("/servlet3");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
